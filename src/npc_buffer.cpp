192208e4a23e2a365005fd7335dea5ee7b5ffe2f
locus313 <pat@lo5t.com>
2020-11-16 11:57:48 -0800

    add include ScriptedCreature.h

--- src/npc_buffer.cpp
+++ src/npc_buffer.cpp
@@ -67,6 +67,7 @@ This code and content is released under the [GNU AGPL v3](https://github.com/aze
 #include "ScriptMgr.h"
 #include "Chat.h"
 #include "Player.h"
+#include "ScriptedCreature.h"
 
 bool BFAnnounceModule;
 bool BuffByLevel;
