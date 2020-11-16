16617203fd6b11dbaae46d6d5ee0e7727a114493
locus313 <pat@lo5t.com>
2020-11-16 12:35:10 -0800

    add include ScriptedGossip.h

--- src/npc_buffer.cpp
+++ src/npc_buffer.cpp
@@ -68,6 +68,7 @@ This code and content is released under the [GNU AGPL v3](https://github.com/aze
 #include "Chat.h"
 #include "Player.h"
 #include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 
 bool BFAnnounceModule;
 bool BuffByLevel;
