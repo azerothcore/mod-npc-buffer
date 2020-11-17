cd4054d398db448ef8357b1ba44147b639890a6b
Patrick Lewis <pat@lo5t.com>
2020-11-16 16:50:56 -0800

    fix(build) misc fixes to resolve build errors (#13)
    * add workflow for testing
    
    * add include Chat.h
    
    * move sql file
    
    * update include.sh
    
    * add conf.sh.dist
    
    * add quotes to key words
    
    * move quote
    
    * add include Player.h
    
    * add include ScriptedCreature.h
    
    * add include ScriptedGossip.h
    
    * remove workflow

--- src/npc_buffer.cpp
+++ src/npc_buffer.cpp
@@ -65,6 +65,10 @@ This code and content is released under the [GNU AGPL v3](https://github.com/aze
 
 #include "Config.h"
 #include "ScriptMgr.h"
+#include "Chat.h"
+#include "Player.h"
+#include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 
 bool BFAnnounceModule;
 bool BuffByLevel;
